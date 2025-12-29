b83499976e854e4136580e8d8be6ef488e342fcb
Wojciech Pietrzok <kosciak@kosciak.net>
2025-12-29 13:35:48 +0100

    colors-256.sh  - rework

--- colors-256.sh
+++ colors-256.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 for fgbg in 38 48 ; do # Foreground / Background
-    for color in {0..255} ; do # Colors
+    for color in {0..15} ; do # Colors
         # Display the color
         printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
         # Display 6 colors per lines
         if [ $((($color + 1) % 8)) == 0 ] ; then
+            echo # New line
+        fi
+    done
+    echo
+    for color in {16..231} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color - 3) % 6)) == 0 ] ; then
+            echo # New line
+        fi
+    done
+    echo
+    for color in {232..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color - 3) % 6)) == 0 ] ; then
             echo # New line
         fi
     done
